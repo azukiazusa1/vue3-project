b5d41e8a70e590024149ee4abf4ebaadcdeeb283
azukiazusa1 <cycleippei@gmail.com>
2020-10-18 12:50:19 +0900

    commit

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,25 +1,36 @@
 <template>
   <input type="text" v-model="state.inputValue" />
-  <button @click="onClick">追加</button>
+  <button @click="onClick" :disabled="state.hasError">追加</button>
+  <p v-if="state.hasError" class="error">タイトルが長すぎ！</p>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, watchEffect } from 'vue'
 
 interface State {
   inputValue: string;
+  hasError: boolean;
 }
 export default defineComponent({
   emits: ['add'],
   setup (_, context) {
     const state = reactive<State>({
-      inputValue: ''
+      inputValue: '',
+      hasError: false
     })
 
     const onClick = () => {
       context.emit('add', state.inputValue)
       state.inputValue = ''
     }
+
+    watchEffect(() => {
+      if (state.inputValue.length > 10) {
+        state.hasError = true
+      } else {
+        state.hasError = false
+      }
+    })
 
     return {
       state,
@@ -28,3 +39,9 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
